ed1fcd948415812eeeb26e3fe83d6e8a76245544
Fontes21 <gabriel-santos619@hotmail.com>
2023-12-05 11:25:27 -0300

    init

---
+++ sair.php
@@ -0,0 +1,10 @@
+<?php
+
+    if (isset($_COOKIE["nome"]))
+    {
+        unset($_COOKIE["nome"]);
+        setcookie("nome", "", time() - 3600, "/");
+    }
+
+    header("Location: acesso.php");
+?>
